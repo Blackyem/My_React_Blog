11f2141c02e135670e574d57494e8044cd02dbe8
yelpcamp-profile <kolayem44@gmail.com>
2023-01-16 22:13:27 +0000

    Deploy React app to GitHub Pages

---
+++ precache-manifest.b63b1610e388cd59f113c8703beb5a0a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1bacf7f76527ee2a0646",
+    "url": "/My_React_Blog/static/css/main.83531a2b.chunk.css"
+  },
+  {
+    "revision": "1bacf7f76527ee2a0646",
+    "url": "/My_React_Blog/static/js/main.1bacf7f7.chunk.js"
+  },
+  {
+    "revision": "06d0d8bffb5b16ef93f4",
+    "url": "/My_React_Blog/static/js/1.06d0d8bf.chunk.js"
+  },
+  {
+    "revision": "ef11c6f1b71a9cba6dbd",
+    "url": "/My_React_Blog/static/js/runtime~main.ef11c6f1.js"
+  },
+  {
+    "revision": "6877f4f1bde66093615d0b79ca24783f",
+    "url": "/My_React_Blog/index.html"
+  }
+];
